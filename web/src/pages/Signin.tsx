0a3d31751e65910464a3daefb652ae8578b37e88
comedu <comeduschool@gmail.com>
2022-06-07 02:53:43 +0900

    Frontend] signup from component - signup form events    - submit request

--- web/src/pages/Signin.tsx
+++ web/src/pages/Signin.tsx
@@ -1,15 +1,29 @@
+// React modules
+import { useEffect } from "react";
+import { useNavigate } from 'react-router-dom';
+import { useCookies } from 'react-cookie';
+
 // Components
 import SigninForm from "../components/signin-form/SigninForm";
 
 const Signin = () => {
-    return (
-      <div className="center">
-        <SigninForm />
-        <div className="center-item">
-          계정이 없으신가요? <a className="signup-link noline-link" href="/signup">가입하기</a>
-        </div>
+  const [cookies] = useCookies();
+  const nav = useNavigate();
+
+  useEffect(()=>{
+    if (cookies.csrftoken) {
+      nav('/', {replace: true});
+    }
+  })
+
+  return (
+    <div className="center">
+      <SigninForm />
+      <div className="center-item">
+        계정이 없으신가요? <a className="signup-link noline-link" href="/signup">가입하기</a>
       </div>
-    );
-  }
+    </div>
+  );
+}
   
-  export default Signin
+export default Signin
